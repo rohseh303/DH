e90653ddf56f059ea8a90ca6361f52eb73c54f42
rohseh303 <rohansehgal935@gmail.com>
2022-12-31 18:11:35 -0800

    got magnify and drag gestures to work together in pinch_zoom test, need to implement in app

--- pinch_zoom_test/pinch_zoom_test/ContentView.swift
+++ pinch_zoom_test/pinch_zoom_test/ContentView.swift
@@ -8,36 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    var body: some View {
+     
+     CircleView()
+     
+    }
+}
+
+struct CircleView: View {
+
+@State private var location: CGSize = CGSize()
+@GestureState private var translation: CGSize = CGSize()
     @State private var offset: CGSize = .zero
     @State private var finalAmount = 1.0
     @State private var curAmount = 0.0
-    @State private var position = CGSize.zero
-    
 
     var body: some View {
-        VStack {
-            //ScrollView {
-                Image("Image")
-                    .scaleEffect(finalAmount + curAmount)
-                    //.offset(offset)
-                    .gesture(
-                        MagnificationGesture()
-                            .onChanged { amount in
-                                curAmount = amount - 1
-                            }
-                            .onEnded{ amount in
-                                finalAmount += curAmount
-                                curAmount = 0
-                            }
-                    )
-                    .gesture(
-                                    DragGesture()
-                                        .onChanged { value in
-                                            self.position = value.translation
-                                        }
-                                )
-            //}
+     
+     let tapDrag = DragGesture(minimumDistance: 0)
+       .updating($translation) { value, state, _ in
+         state = value.translation
+       }
+       .onEnded { value in
+         location = CGSize(width: location.width + value.translation.width, height: location.height + value.translation.height)
+       }
+       .simultaneously(with:                         MagnificationGesture()
+        .onChanged { amount in
+            curAmount = amount - 1
         }
+        .onEnded{ amount in
+            finalAmount += curAmount
+            curAmount = 0
+        }
+  )
+     
+     Image("Image")
+       //.fill()
+       .frame(width: 100, height: 100, alignment: .center)
+       .position(x: location.width + translation.width + 100, y: location.height + translation.height + 100)
+       .scaleEffect(finalAmount + curAmount)
+       .gesture(tapDrag)
     }
 }
 
