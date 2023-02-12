ede1ec652994df7965ff477ec7476595cc740794
ishkhandelwal <ish.khandelwal@gmail.com>
2023-02-12 13:52:10 -0800

    ucr loading and added some restaurants

--- UCLA/UCLA Dining/getFixedMenus/getUCRfixed.swift
+++ UCLA/UCLA Dining/getFixedMenus/getUCRfixed.swift
@@ -8,30 +8,30 @@
 import Foundation
 
 func getUCRfixed() -> [Hall] {
-    let IvansCoffeeShopData = Hall(
+    let IvansatHinderaker = Hall(
         selectedKey: "ucrdiningmenus",
-        name: "Ivan's Coffee Shop",
+        name: "Ivans at Hinderaker",
         //image: "Ivan's Coffee Shop",
-        fixed_menu: ["Ivan's Coffee Shop"],
+        fixed_menu: ["Ivans at Hinderaker Menu 1"],
         sections: 1
     )
     
-    let BytesCafeData = Hall(
+    let BytesData = Hall(
         selectedKey: "ucrdiningmenus",
-        name: "Bytes Cafe",
+        name: "Bytes",
         //image: "Bytes Cafe",
-        fixed_menu: ["Bytes Cafe"],
+        fixed_menu: ["Bytes Menu 1"],
         sections: 1
     )
     let LollicupFreshBobaData = Hall(
         selectedKey: "ucrdiningmenus",
         name: "Lollicup Fresh Boba",
         //image: "Lollicup Fresh Boba",
-        fixed_menu: ["Lollicup Fresh Boba"],
+        fixed_menu: ["Lollicup Fresh Boba Menu 1"],
         sections: 1
     )
 
     
-    let output = [IvansCoffeeShopData, BytesCafeData, LollicupFreshBobaData]
+    let output = [IvansatHinderaker, BytesData,LollicupFreshBobaData]
     return output
 }
