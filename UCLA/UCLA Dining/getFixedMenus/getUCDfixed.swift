eb49c374772f68b7f50f43b13e45fe4e9f227ff4
ishkhandelwal <ish.khandelwal@gmail.com>
2023-02-11 21:42:51 -0800

    added some restaurants

--- UCLA/UCLA Dining/getFixedMenus/getUCDfixed.swift
+++ UCLA/UCLA Dining/getFixedMenus/getUCDfixed.swift
@@ -12,11 +12,17 @@ func getUCDfixed() -> [Hall] {
         selectedKey: "ucddiningmenus",
         name: "Crepe Bistro",
         //image: "Crepe Bistro",
-        fixed_menu: ["Crepe Bistro Menu"],
+        fixed_menu: ["Crepe Bistro Menu 1"],
         sections: 1
     )
-
+    let TheGunrockData = Hall(
+        selectedKey: "ucddiningmenus",
+        name: "The Gunrock",
+        //image: "The Gunrock",
+        fixed_menu: ["The Gunrock Menu 1"],
+        sections: 1
+    )
     
-    let output = [CrepeBistroData]
+    let output = [CrepeBistroData,TheGunrockData]
     return output
 }
