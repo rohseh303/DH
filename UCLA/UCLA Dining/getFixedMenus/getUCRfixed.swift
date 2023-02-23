ab791afb2803e9247c3a41440fc09b3793660c19
ishkhandelwal <ish.khandelwal@gmail.com>
2023-02-22 23:36:30 -0800

    added some restaurrants

--- UCLA/UCLA Dining/getFixedMenus/getUCRfixed.swift
+++ UCLA/UCLA Dining/getFixedMenus/getUCRfixed.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 func getUCRfixed() -> [Hall] {
-    let IvansatHinderaker = Hall(
+    let IvansatHinderakerData = Hall(
         selectedKey: "ucrdiningmenus",
         name: "Ivans at Hinderaker",
         //image: "Ivan's Coffee Shop",
@@ -38,15 +38,44 @@ func getUCRfixed() -> [Hall] {
         sections: 2 ,
         section_names: ["The Grill", "Patio Bar"]
     )
-    let HibachiSanJapaneseGrill = Hall(
+    let HibachiSanJapaneseGrillData = Hall(
         selectedKey: "ucrdiningmenus",
         name: "Hibachi-San Japanese Grill",
         //image: "Hibachi San Japanese Grill",
         fixed_menu: ["Hibachi-San Japanese Grill Menu 1"],
         sections: 1
     )
-
+    let ChronicTacosData = Hall(
+        selectedKey: "ucrdiningmenus",
+        name: "Chronic Tacos",
+        //image: "Chronic Tacos",
+        fixed_menu: ["Chronic Tacos Menu 1"],
+        sections: 1
+    )
+    let HabitData = Hall(
+        selectedKey: "ucrdiningmenus",
+        name: "The Habit Burger Grill",
+        //image: "The Habit Burger Grill",
+        fixed_menu: ["The Habit Burger Grill Menu 1", "The Habit Burger Grill Menu 2", "The Habit Burger Grill Menu 3", "The Habit Burger Grill Menu 4", "The Habit Burger Grill Menu 5", "The Habit Burger Grill Menu 6", "The Habit Burger Grill Menu 7"],
+        sections: 7 ,
+        section_names: ["Family Bundles", "Charburgers", "Signature Sandwiches", "Popular Meals", "Fresh Salads", "Sides", "Drinks"]
+    )
+    let HalalShackData = Hall(
+        selectedKey: "ucrdiningmenus",
+        name: "The Halal Shack",
+        //image: "Halal Shack",
+        fixed_menu: ["The Halal Shack Menu 1"],
+        sections: 1
+    )
+    let CoffeeBeanData = Hall(
+        selectedKey: "ucrdiningmenus",
+        name: "The Coffee Bean and Tea Leaf",
+        //image: "The Coffee Bean",
+        fixed_menu: ["The Coffee Bean and Tea Leaf Menu 1", "The Coffee Bean and Tea Leaf Menu 2", "The Coffee Bean and Tea Leaf Menu 3", "The Coffee Bean and Tea Leaf Menu 4"],
+        sections: 4 ,
+        section_names: ["Tea", "Espresso", "Ice Blended Drinks", "Coffee"]
+    )
     
-    let output = [IvansatHinderaker, BytesData,LollicupFreshBobaData,TheBarnData, HibachiSanJapaneseGrill]
+    let output = [IvansatHinderakerData, BytesData,LollicupFreshBobaData,TheBarnData, HibachiSanJapaneseGrillData, ChronicTacosData, HabitData, HalalShackData, CoffeeBeanData]
     return output
 }
