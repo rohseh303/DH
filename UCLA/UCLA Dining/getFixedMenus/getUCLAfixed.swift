4c64dbc8bda5e4742f828926f94e4247f27a45fe
ishkhandelwal <ish.khandelwal@gmail.com>
2023-02-26 19:44:13 -0800

    finished restaurants

--- UCLA/UCLA Dining/getFixedMenus/getUCLAfixed.swift
+++ UCLA/UCLA Dining/getFixedMenus/getUCLAfixed.swift
@@ -148,7 +148,84 @@ func getUCLAfixed() -> [Hall] {
         fixed_menu: ["Cafe Synapse Menu 1"],
         sections: 1
     )
+    let PlateiaData = Hall(
+        selectedKey: "diningmenus",
+        name: "Plateia",
+        //image: "The Coffee Bean",
+        fixed_menu: ["Plateia Menu 1", "Plateia Menu 2","Plateia Menu 3","Plateia Menu 4", "Plateia Menu 5","Plateia Menu 6"],
+        sections: 6,
+        section_names: ["Breakfast", "Brunch", "Lunch", "Dinner","Drinks","Lounge"]
+
+    )
+    let TacoBellData = Hall(
+        selectedKey: "diningmenus",
+        name: "Taco Bell",
+        //image: "The Coffee Bean",
+        fixed_menu: ["Taco Bell Menu 1", "Taco Bell Menu 2","Taco Bell Menu 3","Taco Bell Menu 4", "Taco Bell Menu 5","Taco Bell Menu 6","Taco Bell Menu 7"],
+        sections: 7,
+        section_names: ["Combos", "Sides", "Drinks", "Breakfast","Sauces","Veggie Meals", "Group Menu"]
+
+    )
+    let WetzelsData = Hall(
+        selectedKey: "diningmenus",
+        name: "Wetzel's Pretzels",
+        //image: "The Coffee Bean",
+        fixed_menu: ["Wetzel's Menu 1"],
+        sections: 1
+    )
+    let CarlsData = Hall(
+        selectedKey: "diningmenus",
+        name: "Carl's Junior",
+        //image: "The Coffee Bean",
+        fixed_menu: ["Carl's Junior Menu 1", "Carl's Junior Menu 2","Carl's Junior Menu 3","Carl's Junior Menu 4", "Carl's Junior Menu 5","Carl's Junior Menu 6"],
+        sections: 6,
+        section_names: ["Breakfast", "Chicken", "Charbroiled", "Beverages","KidsMenu","Sides and Desserts"]
+    )
+    let MusicCafeData = Hall(
+        selectedKey: "diningmenus",
+        name: "Music Cafe",
+        //image: "The Coffee Bean",
+        fixed_menu: ["Music Cafe Menu 1"],
+        sections: 1
+    )
+    let KerckhoffData = Hall(
+        selectedKey: "diningmenus",
+        name: "Kerckhoff Coffee House",
+        //image: "The Coffee Bean",
+        fixed_menu: ["Kerckhoff Coffee House Menu 1"],
+        sections: 1
+    )
+    let LollicupData = Hall(
+        selectedKey: "diningmenus",
+        name: "Lollicup Express",
+        //image: "The Coffee Bean",
+        fixed_menu: ["Lollicup Express Menu 1"],
+        sections: 1
+    )
+    let PandaExpressData = Hall(
+        selectedKey: "diningmenus",
+        name: "Panda Express",
+        //image: "The Coffee Bean",
+        fixed_menu: ["Panda Express Menu 1", "Panda Express Menu 2", "Panda Express Menu 3", "Panda Express Menu 4", "Panda Express Menu 5"],
+        sections: 5 ,
+        section_names: ["Main Menu", "Entree", "Sides", "Appetizers", "Drinks"]
+    )
+    let VeggieGrillData = Hall(
+        selectedKey: "diningmenus",
+        name: "Veggie Grill",
+        //image: "The Coffee Bean",
+        fixed_menu: ["Veggie Grill Menu 1"],
+        sections: 1
+    )
+    let RubiosData = Hall(
+        selectedKey: "diningmenus",
+        name: "Rubios",
+        //image: "The Coffee Bean",
+        fixed_menu: ["Rubios Menu 1"],
+        sections: 1
+    )
+
     
-    let output = [BruinCafeData, EpicuriaAckermanData, DreyData, RendezvousData, DeNeveLateNightData, TheStudyatHedrickData, SpiceKitchen, BurgerAssembly, EpazoteMexicanGrill, Tossed, Fusion, JimmyData, AndersonData, NorthernData, PizzaFrescaData, SouthernLightsData, YoshinoyaData,SynapseData]
+    let output = [BruinCafeData, EpicuriaAckermanData, DreyData, RendezvousData, DeNeveLateNightData, TheStudyatHedrickData, SpiceKitchen, BurgerAssembly, EpazoteMexicanGrill, Tossed, Fusion, JimmyData, AndersonData, NorthernData, PizzaFrescaData, SouthernLightsData, YoshinoyaData,SynapseData, PlateiaData, TacoBellData,WetzelsData, CarlsData, MusicCafeData,KerckhoffData, LollicupData, PandaExpressData, VeggieGrillData, RubiosData]
     return output
 }
